3b8f897d72c656f7d68731c3c7e4e64bfa6c1f6b
David Refoua <David@Refoua.me>
2026-08-13 17:54:36 +0330

    fix(linux): recover stale primary locks

--- Tools/Controller/cmd/controller/host_instance_other.go
+++ Tools/Controller/cmd/controller/host_instance_other.go
@@ -5,6 +5,8 @@ package main
 import (
 	"errors"
 	"os"
+
+	"golang.org/x/sys/unix"
 )
 
 type fileHostInstanceLock struct {
@@ -20,11 +22,15 @@ func platformTryHostInstanceLock(
 	_ string,
 	path string,
 ) (platformHostInstanceLock, bool, error) {
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0o600)
-	if errors.Is(err, os.ErrExist) {
-		return nil, false, nil
-	}
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0o600)
 	if err != nil {
+		return nil, false, err
+	}
+	if err := unix.Flock(int(file.Fd()), unix.LOCK_EX|unix.LOCK_NB); err != nil {
+		_ = file.Close()
+		if errors.Is(err, unix.EWOULDBLOCK) || errors.Is(err, unix.EAGAIN) {
+			return nil, false, nil
+		}
 		return nil, false, err
 	}
 	return &fileHostInstanceLock{file: file, path: path}, true, nil
@@ -36,7 +42,10 @@ func (lock *fileHostInstanceLock) Close() error {
 	}
 	var err error
 	if lock.file != nil {
-		err = lock.file.Close()
+		err = errors.Join(
+			unix.Flock(int(lock.file.Fd()), unix.LOCK_UN),
+			lock.file.Close(),
+		)
 		lock.file = nil
 	}
 	return errors.Join(err, os.Remove(lock.path))
